34440c4eb4548a9b3707dcdd679b1245a39d0bcc
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-12-04 06:52:16 +0100

    01-TS-Tipos básicos

---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,14 @@
+//? 01-Tipos básicos y conceptos generales
+
+const name: string = 'Neimerc';
+let hpPoints: number | 'FULL' = 95;
+const isAlive: boolean = true;
+
+hpPoints = 'FULL';
+
+console.log({ name, hpPoints, isAlive });
+
+// Indicar este export para evitar el error:
+// Cannot redeclare block-scoped variable 'name'
+// Es un tema propio de Vite, no de TypeScript o Angular.
+export {};
