8581594fdb3815395cd8b4c0e65cb04290d9a834
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-12-05 20:08:12 +0100

    01-TS-Funciones con objetos como argumentos

--- 01-typescript-intro/src/topics/03-functions.ts
+++ 01-typescript-intro/src/topics/03-functions.ts
@@ -22,4 +22,28 @@ console.log({ result });
 console.log({ result2 });
 console.log({ multiplyResult });
 
+// Funciones con objetos como argumentos
+
+interface Character {
+  name: string;
+  hp: number;
+  showHp: () => void;
+}
+
+const healCharacter = (character: Character, amount: number) => {
+  character.hp += amount;
+};
+
+const strider: Character = {
+  name: 'Strider',
+  hp: 50,
+  showHp() {
+    console.log(`Puntos de vida ${this.hp}`);
+  },
+};
+
+healCharacter(strider, 10);
+
+strider.showHp();
+
 export {};
