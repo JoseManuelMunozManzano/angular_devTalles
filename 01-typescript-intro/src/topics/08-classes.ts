044c6f29d6876e0b2917fda916d130e2f18dac8b
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-12-06 16:47:42 +0100

    01-TS-Constructor de una clase

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -4,11 +4,22 @@ export class Person {
   public name: string;
   private address: string;
 
-  constructor() {
-    this.name = 'José M.';
-    this.address = 'New York';
+  constructor(name: string, address: string) {
+    this.name = name;
+    this.address = address;
   }
 }
 
-const ironman = new Person();
+const ironman = new Person('Ironman', 'New York');
 console.log(ironman);
+
+//? Constructor de una clase
+// Forma corta de definir clases en TypeScript
+// En el constructor automáticamente crea las propiedades y le asigna los valores.
+// Importante para la inyección de dependencias.
+export class Person2 {
+  constructor(public name: string, private address: string = 'No Address') {}
+}
+
+const spiderman = new Person2('Spiderman');
+console.log(spiderman);
