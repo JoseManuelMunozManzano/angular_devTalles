181ede24bdab0b8c0121950d820868c84e9a6cb0
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-12-06 06:33:05 +0100

    01-TS-Desestructuración de argumentos y ejercicio

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -3,7 +3,9 @@ import './style.css';
 // import './topics/02-object-interface';
 // import './topics/03-functions';
 // import './topics/04-homework-types';
-import './topics/05-basic-destructuring';
+// import './topics/05-basic-destructuring';
+// import './topics/06-function-destructuring_excercise';
+import './topics/06-function-destructuring_resolve';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 Hola Mundo
---
+++ 01-typescript-intro/src/topics/06-function-destructuring_resolve.ts
@@ -0,0 +1,48 @@
+//? Desestructuración de argumentos
+
+// Con desestructuración
+
+interface Product {
+  description: string;
+  price: number;
+}
+
+const phone: Product = {
+  description: 'Nokia A1',
+  price: 150.0,
+};
+
+const tablet: Product = {
+  description: 'iPad Air',
+  price: 250.0,
+};
+
+interface TaxCalculationOptions {
+  tax: number;
+  products: Product[];
+}
+
+// Solución no muy buena porque si hay muchos campos hay mucha desestructuración
+// function taxCalculation({ products, tax }: TaxCalculationOptions): [number, number] {
+function taxCalculation(options: TaxCalculationOptions): [number, number] {
+  // Mejor hacer aquí la desestructuración
+  const { tax, products } = options;
+
+  let total = 0;
+
+  products.forEach(({ price }) => {
+    total += price;
+  });
+
+  return [total, total * tax];
+}
+
+const shoppingCart = [phone, tablet];
+const tax = 0.15;
+
+const [total, taxTotal] = taxCalculation({ products: shoppingCart, tax });
+
+console.log('Total', total);
+console.log('Tax', taxTotal);
+
+export {};
