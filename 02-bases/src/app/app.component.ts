5ea3f8cd46ca4d39fac82fd2cdae4c4fa9927a18
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-12-08 06:44:50 +0100

    02-bases-Contador

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -1,3 +1,4 @@
+// Este es el componente principal de una app de Angular
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public title: string = 'Hola Mundo';
   public counter: number = 10;
+
+  // No se suele indicar public (es por defecto)
+  // Si es private o static, obviamente si que hay que indicarlo.
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+
+  resetCounter(): void {
+    this.counter = 10;
+  }
 }
